7d62ca9d427a9a9466c346b28ef0205f708d5a29
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2024-01-22 17:08:53 +0900

    feat: 投稿用APIのcreateArticleを追加

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -36,3 +36,24 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (id: string, title: string, content: string): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(`http://localhost:3005/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, createdAt: currentDateTime }),
+  });
+
+  if (!res.ok) {
+    throw new Error("エラーが発生");
+  }
+
+  // ローディングの表示のため意図的に遅延を発生
+  await new Promise((resolve) => setTimeout(resolve, 1500))
+
+  const newArticle = await res.json();
+  return newArticle;
+};
