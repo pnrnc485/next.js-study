2cd8611527955c9df45c321d84f38be739edac96
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2024-03-18 16:04:54 +0900

    feat: 記事ポスト後のローディング時のボタン表示制御

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -9,10 +9,16 @@ const CreateBlogPage = () => {
   const [id, setId] = useState<string>("");
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // ページのリロードを防ぐ
 
+    setLoading(true);
+
     await createArticle(id, title, content);
+
+    setLoading(false);
     router.push("/");
     router.refresh();
   };
@@ -56,7 +62,12 @@ const CreateBlogPage = () => {
 
         <button
           type="submit"
-          className="py-2 px-4 border rounded-md bg-orange-300"
+          className={`py-2 px-4 border rounded-md ${
+            loading
+              ? "bg-orange-300 cursor-not-allowed" // ボタン押せなくして色変える
+              : "bg-orange-400 hover:bg-orange-500"
+          }`}
+          disabled={loading}
         >
           投稿
         </button>
